6d7407360f7b34a660aeab34d061de51d88ac83a
Neha Sontakke <nsontakke004@gmail.com>
2021-06-05 16:14:54 +0530

    Update style.js

--- assets/style.js
+++ assets/style.js
@@ -44,7 +44,7 @@ const namefontsize = "23pt";
 
 // Publication properties
 const ptitlecolor = "#003366";
-const ptitlefontsize = bodyfontsize;
+const ptitlefontsize = "13pt";
 const ptitleweight = 500;
 const ptitledecoration = "none";
 const ptitlestyle = "normal";
